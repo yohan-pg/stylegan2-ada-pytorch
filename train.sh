ad0b339ae4e0cc6fa8b166663b2c5a253b204224
yopog <yopog@tchaikovsky.gel.ulaval.ca>
2021-08-26 09:20:00 -0400

    tch-speficis

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
-# python train.py --outdir=./training-runs --data=./datasets/afhq64cat.zip --gpus=2 --use_adaconv True --dry-run
-export TORCH_EXTENSIONS_DIR=extensions
-export CUDA_VISIBLE_DEVICES=1
-export CXX=g++
+# * Note that on tch, the device # appears reversed (titan v is 1, titant rtx is 0)
 
-python train.py --outdir=./training-runs --data=./datasets/afhq64cat.zip --gpus=1 
+export CXX=g++
+export CUDA_VISIBLE_DEVICES=0,1
+
+python train.py --outdir="./training-runs/$1" --kimg 5000 --data=./datasets/afhq128cat.zip --gpus=2 
