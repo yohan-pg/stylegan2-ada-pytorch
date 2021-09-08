9194a22e41e6446e20d7788e30de474b4fb44e67
yopog <yopog@tchaikovsky.gel.ulaval.ca>
2021-09-08 00:55:28 -0400

    added image grid with random noise

--- training/training_loop.py
+++ training/training_loop.py
@@ -296,17 +296,31 @@ def training_loop(
             drange=[0, 255],
             grid_size=grid_size,
         )
-        grid_z = torch.randn([labels.shape[0], G.z_dim], device=device).split(batch_gpu)
-        grid_c = torch.from_numpy(labels).to(device).split(batch_gpu)
-        images = torch.cat(
-            [G_ema(z=z, c=c, noise_mode="const").cpu() for z, c in zip(grid_z, grid_c)]
-        ).numpy()
-        save_image_grid(
-            images,
-            os.path.join(run_dir, "fakes_init.png"),
-            drange=[-1, 1],
-            grid_size=grid_size,
-        )
+        
+        with torch.no_grad():
+            grid_z = torch.randn([labels.shape[0], G.z_dim], device=device).split(batch_gpu)
+            grid_c = torch.from_numpy(labels).to(device).split(batch_gpu)
+            
+            images = torch.cat(
+                [G_ema(z=z, c=c, noise_mode="const").cpu() for z, c in zip(grid_z, grid_c)] 
+            ).numpy()
+            save_image_grid(
+                images,
+                os.path.join(run_dir, "fakes_init.png"),
+                drange=[-1, 1],
+                grid_size=grid_size,
+            )
+
+            images = torch.cat(
+                [G_ema(z=z, c=c, noise_mode="random").cpu() for z, c in zip(grid_z, grid_c)] 
+            ).numpy()
+            save_image_grid(
+                images,
+                os.path.join(run_dir, "random_fakes_init.png"),
+                drange=[-1, 1],
+                grid_size=grid_size,
+            )
+        
 
     # Initialize logs.
     if rank == 0:
@@ -486,18 +500,32 @@ def training_loop(
             and (image_snapshot_ticks is not None)
             and (done or cur_tick % image_snapshot_ticks == 0)
         ):
-            images = torch.cat(
-                [
-                    G_ema(z=z, c=c, noise_mode="const").cpu()
-                    for z, c in zip(grid_z, grid_c)
-                ]
-            ).numpy()
-            save_image_grid(
-                images,
-                os.path.join(run_dir, f"fakes{cur_nimg//1000:06d}.png"),
-                drange=[-1, 1],
-                grid_size=grid_size,
-            )
+            with torch.no_grad():
+                images = torch.cat(
+                    [
+                        G_ema(z=z, c=c, noise_mode="const").cpu()
+                        for z, c in zip(grid_z, grid_c)
+                    ]
+                ).numpy()
+                save_image_grid(
+                    images,
+                    os.path.join(run_dir, f"fakes{cur_nimg//1000:06d}.png"),
+                    drange=[-1, 1],
+                    grid_size=grid_size,
+                )
+
+                images = torch.cat(
+                    [
+                        G_ema(z=z, c=c, noise_mode="random").cpu()
+                        for z, c in zip(grid_z, grid_c)
+                    ]
+                ).numpy()
+                save_image_grid(
+                    images,
+                    os.path.join(run_dir, f"random_fakes{cur_nimg//1000:06d}.png"),
+                    drange=[-1, 1],
+                    grid_size=grid_size,
+                )
 
         # Save network snapshot.
         snapshot_pkl = None
