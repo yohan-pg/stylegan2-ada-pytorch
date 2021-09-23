560c35d23b23a859a2c0100c615131ce2f796d8c
yopog <yopog@liszt.gel.ulaval.ca>
2021-09-23 12:47:06 -0400

    merge

--- train.sh
+++ train.sh
@@ -1,12 +1,10 @@
 
 python train.py \
     --kimg 5000 \
-    --data=./datasets/afhq64cat.zip \
-    --batch 8 \
-    --metrics=fid50k,pr50k3,ppl2_wend \
+    --data=./datasets/afhq32cat.zip \
+    --batch 8 --metrics=fid50k,pr50k3,ppl2_wend \
     --snap 50 \
     --gamma 10 \
-    --gpus=2 \
-    --fp32 True \
+    --gpus=1 \
     --outdir="./training-runs/$1" \
     "${@:2}"
